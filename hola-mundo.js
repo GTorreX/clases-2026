3179c50783191ed28c68c3655b5def391fb83154
Jose Angel L. <jose_loaiza19@hotmail.com>
2026-01-07 12:37:21 -0500

    nuevo avanza

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,2 +1,11 @@
 let cajaDeAndy = "woody"
 console.log(cajaDeAndy)
+
+let carro = {
+    marca: "Tesla",
+    modelo: "clase S"
+}
+
+let frutasDisponibles = ["manzana", "pera", "uvas"]
+
+// function saludar () {}
